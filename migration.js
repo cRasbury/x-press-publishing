15f9014751f73a1725f530be1c4fd5406e47e478
Colton Rasbury <rasbury.colton@gmail.com>
2024-10-27 23:54:32 -0500

    add Series table to db

--- migration.js
+++ migration.js
@@ -2,3 +2,4 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./database.sqlite')
 
 db.run('CREATE TABLE IF NOT EXISTS Artist (id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, date_of_birth TEXT NOT NULL, biography TEXT NOT NULL, is_currently_employed INTEGER DEFAULT 1)');
+db.run('CREATE TABLE IF NOT EXISTS Series (id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, description TEXT NOT NULL)');
